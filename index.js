76bf879d5c8c31ae7444bacbf01e52473ed31ef1
+ <+>
2024-02-04 01:42:46 -0800

    currency searchable by name or country name

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const list = document.getElementById('currency-list')
 const collection = document.getElementById('currency-collection')
-const button = document.getElementById('new-toy-btn')
 const funds = document.getElementById('funds')
+const search = document.getElementById('search')
+
 let Funds = 0
 
 document.addEventListener('DOMContentLoaded', function(){
+    return fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/rates_of_exchange")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        for(let c of data.data){
+            createCurrencyCard(c)
+        }
+    })
+    .catch(function (error) {
+        console.log("Error message:", error.message);
+        let p = document.createElement('p');
+        p = error.message;
+        console.log(p);
+        document.querySelector('body').append(p);
+    });
+
     Funds = 100
     funds.innerText = `$${Funds}`
+})
+
+search.addEventListener('input', function(){
+    for(let i of list.children){
+        if(i.title.slice(0, search.value.length).toLowerCase() !== search.value.toLowerCase() &&
+        i.id.slice(0, search.value.length).toLowerCase() !== search.value.toLowerCase())
+            i.hidden = true
+        else
+            i.hidden = false
+    }
 })
 
 function hasCurrency(currency){
@@ -17,9 +45,12 @@ function hasCurrency(currency){
 }
 
 const buyMoney = function(USD, data){
+    if(USD === 0 || isNaN(USD))
+        return
     Funds -= USD
     funds.innerText = `$${Funds}`
     let exchangedValue = data.exchange_rate*USD
+    console.log(Number(USD))
     if(hasCurrency(data.currency)){
         const card = collection.querySelector(`#${data.currency}`)
         card.value += exchangedValue
@@ -56,6 +87,8 @@ function createCurrencyCard(data){
         />
     </form>`
     card.classList.add('currencyCard')
+    card.id = data.currency
+    card.title = data.country
     let button = card.querySelector('[name="submit"]')
     button.addEventListener('click', () => buyMoney(card.querySelector(`.input-text`).value, data))
     
@@ -66,22 +99,3 @@ function createCurrencyCard(data){
     card.append(p)
     list.append(card)
 }
-
-button.addEventListener('click', function(){
-    return fetch("https://api.fiscaldata.treasury.gov/services/api/fiscal_service/v1/accounting/od/rates_of_exchange")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        for(let c of data.data){
-            createCurrencyCard(c)
-        }
-    })
-    .catch(function (error) {
-        console.log("Error message:", error.message);
-        let p = document.createElement('p');
-        p = error.message;
-        console.log(p);
-        document.querySelector('body').append(p);
-    });
-})
